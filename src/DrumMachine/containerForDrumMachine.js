dbe85d2f82f0bf54400bcf5993867491da633093
bappyasif <bappyasif@hotmail.com>
2021-08-16 03:22:14 +0600

    a fully functioning drum machine kit

--- src/DrumMachine/containerForDrumMachine.js
+++ src/DrumMachine/containerForDrumMachine.js
@@ -1,46 +1,95 @@
 import React, { Component } from 'react'
 import DrumMachineUsingReact from './usingReact'
 
-export let drumSounds = {
-    q: {
+// export let drumSounds = {
+//     q: {
+//         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+//         name: 'Heater-01'
+//     },
+//     w: {
+//         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+//         name: 'Heater-02'
+//     },
+//     e: {
+//         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+//         name: 'Heater-03'
+//     },
+//     a: {
+//         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+//         name: 'Heater-04'
+//     },
+//     s: {
+//         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+//         name: 'Heater-06'
+//     },
+//     d: {
+//         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+//         name: 'Dsc_oh'
+//     },
+//     z: {
+//         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+//         name: 'Kick_n_Hat'
+//     },
+//     x: {
+//         sound: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+//         name: 'RP4_Kick_01'
+//     },
+//     c: {
+//         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+//         name: 'Cev_H2'
+//     },
+// }
+
+export let drumSounds = [
+    {
+        keypad: 'q',
         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
         name: 'Heater-01'
     },
-    w: {
+    {
+        keypad: 'w',
         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
         name: 'Heater-02'
     },
-    e: {
+    {
+        keypad: 'e',
         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
         name: 'Heater-03'
     },
-    a: {
+    {
+        keypad: 'a',
         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
         name: 'Heater-04'
     },
-    s: {
+    {
+        keypad: 's',
         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
         name: 'Heater-06'
     },
-    d: {
+    {
+        keypad: 'd',
         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
         name: 'Dsc_oh'
     },
-    z: {
+    {
+        keypad: 'z',
         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
         name: 'Kick_n_Hat'
     },
-    x: {
+    {
+        keypad: 'x',
         sound: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
         name: 'RP4_Kick_01'
     },
-    c: {
+    {
+        keypad: 'c',
         sound: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
         name: 'Cev_H2'
     },
-}
+]
 
-export let list = "q,w,e,a,s,d,z,x,c,Q,W,E,A,S,D,Z,X,C";
+// export let list = "q,w,e,a,s,d,z,x,c,Q,W,E,A,S,D,Z,X,C";
+let list = "q,w,e,a,s,d,z,x,c";
 
 class ContainerForDrumMachine extends Component {
     constructor(props) {
